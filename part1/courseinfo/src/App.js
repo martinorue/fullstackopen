a292d9690a2a5b7b97643dd70c9253fb040bc994
martinorue <martin.orue008@gmail.com>
2022-05-12 12:30:33 -0300

    completar courseinfo

---
+++ part1/courseinfo/src/App.js
@@ -0,0 +1,75 @@
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.course.name}</h1>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <>
+      <Part part={props.parts[0]} />
+      <Part part={props.parts[1]} />
+      <Part part={props.parts[2]} />
+    </>
+  )
+}
+
+const Part = (props) => {
+  console.log(props);
+  return (
+    <>
+      <p>
+        {props.part.name} {props.part.exercises}
+      </p>
+      <p>
+
+      </p>
+    </>
+  )
+
+}
+
+
+const Total = (props) => {
+  let total = 0;
+  props.parts.forEach(element => {
+    total += element.exercises
+  });
+  return (
+    <>
+      <p>Number of exercises {total}</p>
+    </>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <>
+      <Header course={course} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </>
+  )
+}
+
+export default App
