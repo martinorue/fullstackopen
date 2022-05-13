6802b4525283dd9b6a2591acab4b0f58b62f84c5
martinorue <martin.orue008@gmail.com>
2022-05-13 15:13:38 -0300

    complete unicafe step1

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
+const Header = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{props.header}</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+const Button = (props) => {
+  return (
+    <>
+      <button onClick={props.handleClick}>
+        {props.text}
+      </button>
+    </>
+  )
+}
+
+const Stats = (props) => {
+  return (
+    <div>
+      {props.text} {props.sentiment}
+    </div>
+  )
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const setToGood = newValue => setGood(newValue);
+  const setToNeutral = newValue => setNeutral(newValue);
+  const setToBad = newValue => setBad(newValue);
+
+  return (
+    <>
+      <Header header={"give feedback"} />
+      <Button handleClick={() => setToGood(good + 1)} text="good" />
+      <Button handleClick={() => setToNeutral(neutral + 1)} text="neutral" />
+      <Button handleClick={() => setToBad(bad + 1)} text="bad" />
+      <Header header={"statistics"} />
+      <Stats text="good" sentiment={good} />
+      <Stats text="neutral" sentiment={neutral} />
+      <Stats text="bad" sentiment={bad} />
+    </>
+  )
+}
+
+export default App
