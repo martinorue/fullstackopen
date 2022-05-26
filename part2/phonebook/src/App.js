18ae5703e68ea87c397a171818b91c501b6e7f46
martinorue <martin.orue008@gmail.com>
2022-05-25 22:35:32 -0300

    filter in progress

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -11,10 +11,14 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
   const [newName, setNewName] = useState('')
   const [newPhone, setNewPhone] = useState('')
+  const [filtered, setFiltered] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -40,12 +44,21 @@ const App = () => {
     setNewPhone(event.target.value);
   }
 
+  const handleFilterChange = (event) => {
+    setFiltered(event.target.value);
+    // const regex = new RegExp( newFilter, 'i' );
+    // const filteredPersons = () => allPersons.filter(person => person.name.match(regex))
+    // setPersons(filteredPersons)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
       <div>
         filter shown with
         <input
+          value={filtered}
+          onChange={handleFilterChange}
         >
         </input>
       </div>
@@ -70,7 +83,9 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map((p, i) => <li key={i}>{p.name} {p.phone}</li>)}
+        {persons.map((p) => p.name.toLowerCase().startsWith(filtered.toLowerCase()) ? 
+        <li key={p.id}>{p.name} {p.phone}</li>: 
+        '')}
       </ul>
     </div>
   )
